cc60887c5b7af68b59a70692f0a0c77f8e563f1a
mllegoman <pixel.x.widow@gmail.com>
2021-09-21 19:23:44 -0400

    mkfont

--- mkfont/mkfont.c
+++ mkfont/mkfont.c
@@ -6,10 +6,7 @@
 
 // TODO:
 // make things faster -- things are getting really slow
-// resize window - prettymuch everything else
 // make clearing individual curves easier
-// unsigned int => unsigned char
-// make writing 'unsigned [int|char]' easier
 
 int adjpow(uint n) {
 char i = 31;
@@ -78,8 +75,12 @@ slice[0] = 1;
 	}
 }
 
-void draw(uchar x[20][20], uchar y[20][20], uchar n[20], char l) {
+void draw(uchar x[20][20], uchar y[20][20], uchar n[20], char l, uint sx, uint sy) {
 RegionFill(0, 0, WW(0), WH(0), RGB(0, 0, 0), 0);
+
+if (sx&&l) {
+RegionScarf(sx-4, sy-4, 10, 10, RGB(255, 255, 255), 0);
+}
 
 char i, j;
 uint wh = WH(0);
@@ -125,18 +126,18 @@ n[i] = 0;
 
 XI(0, "", "", 256, 512, 0, 0, 0);
 Eve(&c, 0);
-draw(x, y, n, l);
+draw(x, y, n, l, 0, 0);
 	while(1) {
 	c.t = 3;
 	printf("requesting alpha:\n");
-		while(c.t!=1) {
+		while(c.t!=1||c.txt>256) {
 		Eve(&c, 0);
 		}
-	alpha = c.txt[0];
+	alpha = c.txt;
 	printf("got our alpha: %c\n", alpha);
-	c.txt[0] = 0;
+	c.txt = 0;
 	m = 0;
-		while(c.txt[0]!=115){
+		while(c.txt!=115){
 			while(Pend(0)){
 			if (Pend(0) > 50) {
 				while(1) {
@@ -148,7 +149,7 @@ draw(x, y, n, l);
 			}
 			Eve(&c, 0);
 				if (c.t==3) {
-				draw(x, y, n, l);
+				draw(x, y, n, l, 0, 0);
 				}
 			
 				if (c.t==2) {
@@ -160,8 +161,11 @@ draw(x, y, n, l);
 							if (tot < 10) {
 							c.t = 3;
 							tmpx = x[i][j];
-							tmpy = y[i][j]; 
+							tmpy = y[i][j];
 								while(c.t!=2) {
+									if (c.t==3) {
+									draw(x, y, n, l, tmpx*WH(0)/256, tmpy*WH(0)/256);
+									}
 								Eve(&c, 0);
 								}
 								// if c.t==1&&c.txt[0]=="c" { clear curve }
@@ -176,19 +180,19 @@ draw(x, y, n, l);
 										}
 									}
 								}
-							draw(x, y, n, l);
+							draw(x, y, n, l, 0, 0);
 							}
 						}
 					}
 				}
 			
 				if (c.t==1) {
-					if (c.txt[0]==113) {
+					if (c.txt==113) {
 					fclose(F);
 					Clean(0);
 					XX(0);
 					}
-					if (c.txt[0]==112) {
+					if (c.txt==112) {
 						for (i = 0; i < 20; i++) {
 							for (j = 0; j < 20; j++) {
 								if (x[i][j]==0) {
@@ -200,19 +204,19 @@ draw(x, y, n, l);
 						}
 					printf("\n");
 					}
-					if (c.txt[0]==110&&m < 20) {
+					if (c.txt==110&&m < 20) {
 					c.t = 3;
 						printf("requesting n:\n");
 						while(c.t!=1) {
 						Eve(&c, 0);
-							if (c.txt[0] > 49&&c.txt[0] < 58) {
-							xn = c.txt[0] - 48;
-							printf("got our n: %d\n", xn);
+							if (c.txt > 49&&c.txt < 74) {
+							xn = c.txt - 48 - 7*(c.txt>58);
+							printf("got our n: %d%d\n", xn/10, xn%10);
 							n[m] = xn;
-								while (c.txt[0]!=13) {
+								while (c.txt!=65293) {
 								Eve(&c, 0);
-									if (c.txt[0] > 48&&c.txt[0] < 49 + xn) {
-									cu = c.txt[0] - 49;
+									if ((c.txt > 48&&c.txt < 49 + xn + (xn > 9) * (20 - xn))||(c.txt >64&&c.txt < 64 + xn%10)) {
+									cu = c.txt - 49 - 7*(c.txt>58);
 									printf("%d\n", cu);
 										while(c.t!=2) {
 										Eve(&c, 0);
@@ -225,14 +229,14 @@ draw(x, y, n, l);
 														if (tot < 10&&m!=i&&cu!=j) {
 														x[m][cu] = x[i][j];
 														y[m][cu] = y[i][j];
-														draw(x, y, n, l);
+														draw(x, y, n, l, 0, 0);
 														}
 													}
 												}
 												if (x[m][cu]==0) {
 												x[m][cu] = c.x*256/WH(0);
 												y[m][cu] = c.y*256/WH(0);
-												draw(x, y, n, l);
+												draw(x, y, n, l, 0, 0);
 												}
 											}
 										}
@@ -243,13 +247,13 @@ draw(x, y, n, l);
 							}
 						}
 					m++;
-					draw(x, y, n, l);
+					draw(x, y, n, l, 0, 0);
 					}
-					if (c.txt[0]==108) {
+					if (c.txt==108) {
 					l = 1 - l;
-					draw(x, y, n, l);
+					draw(x, y, n, l, 0, 0);
 					}
-					if (c.txt[0]==99) {
+					if (c.txt==99) {
 					m = 0;
 						for (i = 0; i < 20; i++) {
 						n[i] = 0;
@@ -258,7 +262,7 @@ draw(x, y, n, l);
 							y[i][j] = 0;
 							}
 						}
-					draw(x, y, n, l);
+					draw(x, y, n, l, 0, 0);
 					}
 				}
 			}
@@ -284,6 +288,6 @@ draw(x, y, n, l);
 			y[i][j] = 0;
 			}
 		}
-	draw(x, y, n, l);
+	draw(x, y, n, l, 0, 0);
 	}
 }
