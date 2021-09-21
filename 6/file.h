8b46413d7c4a3a0e135e9a9c3a1e69a1d8416b27
mllegoman <pixel.x.widow@gmail.com>
2021-09-21 19:17:39 -0400

    flushings out

---
+++ 6/file.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+struct getdim {
+unsigned int h;
+unsigned int w;
+};
+extern struct getdim hw(char win);
+extern void setdim(unsigned int h, unsigned int w, char win);
+extern FILE *opensessionfile(char sesid);
+extern void hauldata(char sesid, int date, char p);
+extern void funnel(char sesid, char base[]);
+extern char *getfont(char *fontn, char n);
+unsigned int getlastused();
+void sign(unsigned int date);
